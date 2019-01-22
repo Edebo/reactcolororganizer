493485458813d9cac1fb040125d1451eb78afbba
Edebo <akandeadebowale0@gmail.com>
2019-01-22 11:07:09 +0100

    Updates

---
+++ precache-manifest.720715b417d68d9dbd47618027e8ac73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dd4ca397c443b719ab1",
+    "url": "/reactcolororganizer/static/css/main.d7ddec50.chunk.css"
+  },
+  {
+    "revision": "6dd4ca397c443b719ab1",
+    "url": "/reactcolororganizer/static/js/main.6dd4ca39.chunk.js"
+  },
+  {
+    "revision": "89efe55ed425d2e2d1a4",
+    "url": "/reactcolororganizer/static/js/1.89efe55e.chunk.js"
+  },
+  {
+    "revision": "528a647611d57308dc41",
+    "url": "/reactcolororganizer/static/js/runtime~main.528a6476.js"
+  },
+  {
+    "revision": "99e57ef91ab43188b5448e9d8ae7c918",
+    "url": "/reactcolororganizer/index.html"
+  }
+];
